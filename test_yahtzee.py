602dc1271a76519e8bc2d0a6cf01da5b9d1d3b9f
Axel CHENU <chenu.axel26@gmail.com>
2022-04-22 17:11:23 +0200

    add test_yahtzee.py

---
+++ test_yahtzee.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, main, mock, main
+from unittest.mock import MagicMock
+from yahtzee import yahtzee
+
+
+class test_mock(TestCase):
+    def test_yatzy_scores_50():
+        expected = 50
+        actual = Yatzy.yatzy([4,4,4,4,4])
+        assert expected == actual
+        assert 50 == Yatzy.yatzy([6,6,6,6,6])
+        assert 0 == Yatzy.yatzy([6,6,6,6,3])
+    
