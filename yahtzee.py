116950f1f4bd639019ac5178ad2ec21b014b6b45
Axel CHENU <chenu.axel26@gmail.com>
2022-04-22 17:21:01 +0200

    update yahtzee.py

--- yahtzee.py
+++ yahtzee.py
@@ -1,6 +1,6 @@
 class yahtzee:
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
+    def chance(D1, D2, D3, D4, D5):
         total = 0
         total += d1
         total += d2
@@ -10,3 +10,47 @@ class yahtzee:
         return total
 
 
+    @staticmethod
+    def ones(D1, D2, D3, D4, D5):
+        sum = 0
+        if (D1 == 1):
+            sum += 1
+        if (D2 == 1):
+            sum += 1
+        if (D3 == 1):
+            sum += 1
+        if (D4 == 1):
+            sum += 1
+        if (D5 == 1): 
+            sum += 1
+        return sum
+
+    @staticmethod
+    def two (D1, D2, D3, D4, D5):
+        sum = 0
+        if (D1 == 2):
+            sum += 2
+        if (D2 == 2):
+            sum += 2
+        if (D3 == 2):
+            sum += 2
+        if (D4 == 2):
+            sum += 2
+        if (D5 == 2): 
+            sum += 2
+        return sum
+
+    @staticmethod
+    def three (D1, D2, D3, D4, D5):
+        sum = 0
+        if (D1 == 3):
+            sum += 3
+        if (D2 == 3):
+            sum += 3
+        if (D3 == 3):
+            sum += 3
+        if (D4 == 3):
+            sum += 3
+        if (D5 == 3): 
+            sum += 3
+        return sum
