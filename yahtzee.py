6354d60b1d06efa9a1c46c166f68b9f4cfdcb697
Axel CHENU <chenu.axel26@gmail.com>
2022-04-22 18:14:09 +0200

    update yahtzee.py

--- yahtzee.py
+++ yahtzee.py
@@ -1,14 +1,5 @@
 class yahtzee:
-    @staticmethod
-    def chance(D1, D2, D3, D4, D5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
-
+    
     def __init__(self, D1, D2, D3, D4, D5):
         self.dice = [0]*5
         self.dice[0] = D1
@@ -17,6 +8,8 @@ class yahtzee:
         self.dice[3] = D4
         self.dice[4] = D5
 
+#### Upper section combinaisons
+#ones
     @staticmethod
     def ones(D1, D2, D3, D4, D5):
         sum = 0
@@ -32,6 +25,7 @@ class yahtzee:
             sum += 1
         return sum
 
+#twos
     @staticmethod
     def two (D1, D2, D3, D4, D5):
         sum = 0
@@ -47,6 +41,7 @@ class yahtzee:
             sum += 2
         return sum
 
+#threes
     @staticmethod
     def three (D1, D2, D3, D4, D5):
         sum = 0
@@ -62,7 +57,8 @@ class yahtzee:
             sum += 3
         return sum
 
-    def fours(self):
+#fours
+    def four(self):
         sum = 0
         i = 0
         for at in range(len(self.dice)):
@@ -70,8 +66,8 @@ class yahtzee:
                 sum += 4
         return sum
     
-
-    def fives(self):
+#fives
+    def five(self):
         sum = 0
         i = 0
         for i in range(len(self.dice)): 
@@ -79,11 +75,79 @@ class yahtzee:
                 sum = sum + 5
         return sum
     
-
-    def sixes(self):
+#sixes
+    def sixe(self):
         sum = 0
         i = 0
         for at in range(len(self.dice)): 
             if (self.dice[i] == 6):
                 sum = sum + 6
         return sum
+ 
+
+#### Lower section combinations
+# chance
+    @staticmethod
+    def chance(D1, D2, D3, D4, D5):
+        total = 0
+        total += d1
+        total += d2
+        total += d3
+        total += d4
+        total += d5
+        return total
+# yahtzee
+    @staticmethod
+    def yatzee(dice):
+        counts = [0]*(len(dice)+1)
+        for d in dice:
+            counts[d-1] += 1
+        for i in range(len(counts)):
+            if counts[i] == 5:
+                return 50
+        return 0
+# fullHouse
+    @staticmethod
+    def fullHouse(D1, D2, D3, D4, D5):
+        equivalent = []
+        _2 = False
+        i = 0
+        _2_at = 0
+        _3 = False
+        _3_at = 0
+
+        equivalent = [0]*6
+        equivalent[D1-1] += 1
+        equivalent[D2-1] += 1
+        equivalent[D3-1] += 1
+        equivalent[D4-1] += 1
+        equivalent[D5-1] += 1
+
+        for i in range(6):
+            if (equivalent[i] == 2): 
+                _2 = True
+                _2_at = i+1
+            
+
+        for i in range(6):
+            if (equivalent[i] == 3): 
+                _3 = True
+                _3_at = i+1
+            
+
+        if (_2 and _3):
+            return _2_at * 2 + _3_at * 3
+        else:
+            return 0
+
+# three_kind
+
+# four_kind
+
+# small_straight
+
+# large_straight
+
+# one_pair
+
+#two_pairs
