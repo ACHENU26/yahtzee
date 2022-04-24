ff025878a1a793c32fe2fd7da376e4ff5e5303c9
Axel CHENU <chenu.axel26@gmail.com>
2022-04-24 14:07:03 +0200

    2nd test : sum 6

--- test_yahtzee.py
+++ test_yahtzee.py
@@ -7,6 +7,9 @@ class test_yatzee(TestCase):
     def test_sum_1(self):
         assert Yahtzee.Turn([1, 2, 3, 4, 5], 1) == 1
 
+    def test_sum_6(self):
+        assert Yahtzee.Turn([1, 2, 6, 5, 6], 6) == 12
+
 
 
 
--- yahtzee.py
+++ yahtzee.py
@@ -1,4 +1,23 @@
+def count(expected, roll):
+    return roll.count(expected)
+
+
+def more_occurence(roll):
+    return max(roll, key=roll.count)
+
+
+def sum_if_valid(roll, required):
+    maxNumber = more_occurence(roll)
+    if count(maxNumber, roll) == required:
+        if required == 5:
+            return 50
+        else:
+            return sum(roll)
+    else:
+        return 0
+
+
 class Yahtzee:
     @classmethod
     def Turn(cls, roll, expected):
-        return 1
+        return count(expected, roll) * expected
