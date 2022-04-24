8646fc034f58b6edf6d1a21b509865e2f4a58c0c
Axel CHENU <chenu.axel26@gmail.com>
2022-04-24 14:10:40 +0200

    3rd test : sum invalid

--- test_yahtzee.py
+++ test_yahtzee.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import TestCase
 from yahtzee import Yahtzee
 
-#### Upper section combinaisons tests
+
 class test_yatzee(TestCase):
     def test_sum_1(self):
         assert Yahtzee.Turn([1, 2, 3, 4, 5], 1) == 1
@@ -10,11 +10,11 @@ class test_yatzee(TestCase):
     def test_sum_6(self):
         assert Yahtzee.Turn([1, 2, 6, 5, 6], 6) == 12
 
+    def test_sum_Invalid(self):
+        assert Yahtzee.Turn([1, 2, 6, 5, 6], 3) == 0
 
 
 
-
-#### Lower section combinaisons tests
 
 if __name__ == '__main__':
     unittest.main()
